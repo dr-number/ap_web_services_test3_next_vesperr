3bf208166ab38c715ae6ffc861ffbb9f10165f36
pc-97 <dr.number@yandex.ru>
2021-07-01 21:38:22 +0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
+import '../src/assets/vendor/aos/aos.css';
+import '../src/assets/vendor/bootstrap/css/bootstrap.min.css';
+import '../src/assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import '../src/assets/vendor/boxicons/css/boxicons.css';
+import '../src/assets/vendor/glightbox/css/glightbox.min.css';
+import '../src/assets/vendor/remixicon/remixicon.css';
+import '../src/assets/vendor/swiper/swiper-bundle.min.css';
+
+import '../src/assets/css/style.css';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
